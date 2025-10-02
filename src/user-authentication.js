c74f552f8369cfaee7e6bcae7f865e24c8f60dab
s338605 <s338605@studenti.polito.it>
2025-10-02 20:54:48 +0200

    Help me2

--- src/user-authentication.js
+++ src/user-authentication.js
@@ -66,6 +66,7 @@ const initAuthentication=(app) =>{
     cookie: {
       secure: process.env.NODE_ENV === "production",
       httpOnly: true,
+      sameSite:"none",
       maxAge: 1000 * 60 * 60 * 12 // 12 ore (in ms)
     }
   }));
